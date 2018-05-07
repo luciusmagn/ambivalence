f82df61afce1d8e231c41b75425f16c917a697c7
Lukáš Hozda <luk.hozda@gmail.com>
2018-05-07 15:26:31 +0200

    add update.sh, finish shard's refute

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ansi_czech; cargo build --release; cd ..
-watch -n 1 "ansi_czech/target/release/ansi_czech ambivalence.me | pdfroff -me - > ambivalence.pdf"
+echo updating...
+ansi_czech/target/release/ansi_czech ambivalence.me | pdfroff -me - > ambivalence.pdf
 
