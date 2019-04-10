da8d44b40828dc911ab7b8c240bded7b65d2e754
Lukáš Hozda <luk.hozda@gmail.com>
2019-04-10 22:41:30 +0200

    we are making references now, baybee

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo updating...
-ascii_czech/target/release/ascii_czech $1.me | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
+indxbib refer
+ascii_czech/target/release/ascii_czech $1.me | pic | eqn | refer -prefer | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
