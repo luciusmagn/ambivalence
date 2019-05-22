9d29ee7b60cae4a4125d0516f3b4b7fdd8310419
Lukáš Hozda <luk.hozda@gmail.com>
2019-05-22 15:14:06 +0200

    üpdate guns

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo updating...
 indxbib refer
-# refer -prefer $1.me |
-ascii_czech/target/release/ascii_czech $1.me | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
+refer -prefer $1.me | ascii_czech/target/release/ascii_czech - | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
