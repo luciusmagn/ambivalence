2d1b85fc32e095b2105efff81e1a77a3e1c5cf8f
Lukáš Hozda <luk.hozda@gmail.com>
2019-06-02 21:33:57 +0200

    cumulative update

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo updating...
 indxbib refer
-refer -prefer $1.me | ascii_czech/target/release/ascii_czech - | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
+# refer -prefer $1.me |
+cat $1.me | ascii_czech/target/release/ascii_czech - | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
