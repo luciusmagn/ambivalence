98eac5c5183ba31104e519bb55bba62657c8912a
Lukáš Hozda <luk.hozda@gmail.com>
2019-06-04 07:43:25 +0200

    lel

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 echo updating...
 indxbib refer
 indxbib dark_souls_literatura
-ascii_czech/target/release/ascii_czech $1.me | refer -prefer |  ascii_czech/target/release/ascii_czech - | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
+ascii_czech/target/release/ascii_czech $1.me | refer -prefer -pdark_souls_literatura |  ascii_czech/target/release/ascii_czech - | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
