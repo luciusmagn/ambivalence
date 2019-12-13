ea2152d46120596ce4984ce75c16fbc5e643f265
Lukáš Hozda <luk.hozda@gmail.com>
2019-12-13 13:32:42 +0100

    cumulative update

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo updating...
-indxbib refer
-indxbib dark_souls_literatura
-indxbib ai
-ascii_czech/target/release/ascii_czech $1.me | refer -pai -prefer -pdark_souls_literatura |  ascii_czech/target/release/ascii_czech - | pic | eqn | pdfroff -dpaper=a4 -P-pa4 -me - > $1.pdf; fish --command "kill -s SIGHUP mupdf; exit"
+indxbib refer_dbs/refer
+indxbib refer_dbs/dark_souls_literatura
+indxbib refer_dbs/ai
+
+#cat $1.me \
+ascii_czech/target/release/ascii_czech $1.me \
+	| refer -p "refer_dbs/ai" -p "refer_dbs/refer" -p "refer_dbs/dark_souls_literatura" \
+	| ascii_czech/target/release/ascii_czech - \
+	| pic \
+	| eqn \
+	| groff -dpaper=a4 -P-pa4 -me - \
+	| ps2pdf - > $1.pdf
+
+fish --command "kill -s SIGHUP mupdf; exit"
+
