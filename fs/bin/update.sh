5d5ca663121d6d2a2e2dc37e7a4d59757b730733
Lukáš Hozda <luk.hozda@gmail.com>
2020-03-06 22:16:50 +0100

    dark refer magic

--- fs/bin/update.sh
+++ fs/bin/update.sh
@@ -9,6 +9,7 @@ echo updating...
 #mkey fs/share/refer/Ind | fs/ucblib/reftools/inv fs/share/refer/papers/Ind
 
 mkdir _refer_dbs
+mkdir .refer_dbs
 
 /usr/bin/indxbib refer_dbs/ai
 /usr/bin/indxbib refer_dbs/refer
@@ -18,14 +19,16 @@ rm -rf _refer_dbs
 
 if [[ -z "$TMAC" ]]; then
 	export TMAC=me
-fi 
+fi
+
+fd . refer_dbs/ -x sh -c "cat {} | ascii_czech fuck - > .refer_dbs/{/}"
 
 cat $(basename -- "$1" .$TMAC).$TMAC \
 	| pic \
 	| eqn \
 	| grap \
 	| ascii_czech fuck - \
-	| /usr/bin/refer -p "refer_dbs/refer" -p "refer_dbs/ai" -p "refer_dbs/dark_souls_literatura" \
+	| refer -p ".refer_dbs/refer" -p ".refer_dbs/ai" -p ".refer_dbs/dark_souls_literatura" \
 	| ascii_czech unfuck - \
 	| if [[ "$DEBUG" = "true" ]]; then tee /dev/stderr; else cat - ; fi \
 	| troff -mg -mpictures -dpaper=a4 "-$TMAC" - \
